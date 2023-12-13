378546f91285642554fa330dedcae960b9381f71
github_cloudlabsuser_071 <80262953+github-cloudlabsuser-071@users.noreply.github.com>
2023-12-13 16:27:03 +0000

    Copilot fourth commit

---
+++ test.test.js
@@ -0,0 +1,9 @@
+function multiply(a, b) {
+    return a * b;
+}
+
+// BEGIN: Test multiply function
+console.log(multiply(2, 3)); // Expected output: 6
+console.log(multiply(5, 0)); // Expected output: 0
+console.log(multiply(-4, 7)); // Expected output: -28
+// END: Test multiply function
